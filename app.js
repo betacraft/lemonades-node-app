ba53e6647a4323e26aca210bb2449d66c1d6704f
Akshay Deo <akshay@rainingclouds.com>
2015-04-26 15:53:26 +0530

    - added port config for heroku

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html')
 });
 
-var server = app.listen(3000, function () {
+var port = process.env.PORT || 3000;
+var server = app.listen(port, function () {
 
     var host = server.address().address;
     var port = server.address().port;
