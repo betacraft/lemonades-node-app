25ed074541f53157a1f700dc7452e2a5f085e2f7
Akshay Deo <akshay@rainingclouds.com>
2015-05-03 17:12:59 +0530

    - added new version

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
 var express = require('express');
+var phantomExpress = require("phantom-express");
+
 var app = express();
 
-app.use(express.static(__dirname + '/'));
+var options = {
+    // Currently the middleware caches the response from the
+    // phantomjs process in the memory. The parameter defines
+    // TTL in seconds. If 0 is passed the cache will be ignored.
+    cacheLifetime: 3600,
+    // Dump status to the console or not
+    verbose: false,
+    // Prepends the string to the pretty generated hash
+    // ex. if '!' is defined ->  #!/home/page
+    hashPrepend: ''
+};
 
+app.use(express.static(__dirname + '/'));
+app.use(express.query());
+app.use(phantomExpress(options));
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html')
 });
