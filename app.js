9e5f680263ee364d153ef35b897ef5a33c4950c7
Akshay Deo <akshay@rainingclouds.com>
2015-05-03 18:18:43 +0530

    - added node-phantom as well

--- app.js
+++ app.js
@@ -1,33 +1,33 @@
 var express = require('express');
 var phantomExpress = require("phantom-express");
-
 var app = express();
-
 var options = {
     // Currently the middleware caches the response from the
     // phantomjs process in the memory. The parameter defines
     // TTL in seconds. If 0 is passed the cache will be ignored.
     cacheLifetime: 3600,
     // Dump status to the console or not
-    verbose: false,
+    verbose: true,
     // Prepends the string to the pretty generated hash
     // ex. if '!' is defined ->  #!/home/page
     hashPrepend: '!'
 };
 
-app.use(express.static(__dirname + '/'));
+app.use('/styles', express.static(__dirname + '/styles'));
+app.use('/scripts', express.static(__dirname + '/scripts'));
+app.use('/views', express.static(__dirname + '/views'));
+app.use('/images', express.static(__dirname + '/images'));
 app.use(express.query());
 app.use(phantomExpress(options));
+
 app.get('/', function (req, res) {
+    console.log("got request", req);
     res.sendFile(__dirname + '/index.html')
 });
 
 var port = process.env.PORT || 9000;
 var server = app.listen(port, function () {
-
     var host = server.address().address;
     var port = server.address().port;
-
-    console.log('Example app listening at http://%s:%s', host, port);
-
+    console.log('Lemonades web-ui listening at http://%s:%s', host, port);
 });
